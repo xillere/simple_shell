99847081e1121d99d9a492bf8e3150320c72ec1b
Elijah Mukoro <elijahmukoro7@gmail.com>
2023-09-15 01:52:14 -0700

    header

--- shell.h
+++ shell.h
@@ -6,6 +6,14 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <stddef.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <limits.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <stdbool.h>
 
+#define BUFFER_SIZE 1024
 
 #endif
