568a68e97b7e55fad9e8bb225a39f2cddc477281
Elijah Mukoro <elijahmukoro7@gmail.com>
2023-09-17 10:03:43 -0700

    tools

--- shell.h
+++ shell.h
@@ -16,4 +16,9 @@
 
 #define BUFFER_SIZE 1024
 
+int _strlen(char *s);
+int _strcmp(char *s1, char *s2);
+char *_strcat(char *dest, char *src);
+char *_memset(char *s, char b, unsigned int n);
+
 #endif
