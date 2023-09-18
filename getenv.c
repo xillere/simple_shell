7489ba9e086a899443db6cab7aa5b07590f64ef0
Elijah Mukoro <elijahmukoro7@gmail.com>
2023-09-18 05:03:39 -0700

    corre

--- getenv.c
+++ getenv.c
@@ -1,7 +1,28 @@
-int main()
+#include "shell.h"
+
+extern char **environ;
+/**
+ * _getenv - prints the environment
+ * @name: string
+ * Return: Always 0.
+ */
+char *_getenv(const char *name)
 {
-	char *str = _getenv("LESSCLOSE");
-	
-	printf("%s\n",str);
-	return 0;
+	unsigned int i;
+	char *absvar;
+	char *value;
+
+	i = 0;
+	while (environ[i] != NULL)
+	{
+		if (_strncmp(name, environ[i], _strlen(name)) == 0)
+		{
+			absvar = environ[i] + _strlen(name) + 1;
+			value = malloc(sizeof(char) * _strlen(absvar) + 1);
+			_strcpy(value, absvar);
+			return (value);
+		}
+		i++;
+	}
+	return (NULL);
 }
