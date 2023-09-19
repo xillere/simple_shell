61d42c558773c52bf8da23da154806c87ff7ea5d
Elijah Mukoro <elijahmukoro7@gmail.com>
2023-09-19 12:18:55 -0700

    spaces

--- tokening.c
+++ tokening.c
@@ -8,19 +8,19 @@
  **/
 char **tokenize(char *b, char *delim)
 {
-        int i;
-        char *tok;
-        char **tokens;
+	int i;
+	char *tok;
+	char **tokens;
 
-        tokens = malloc(sizeof(char *) * 64);
-        i = 0;
-        tok = strtok(b, delim);
-        while (tok != NULL)
-        {
-                tokens[i] = tok;
-                tok = strtok(NULL, delim);
-                i++;
-        }
-        tokens[i] = NULL;
-        return (tokens);
+	tokens = malloc(sizeof(char *) * 64);
+	i = 0;
+	tok = strtok(b, delim);
+	while (tok != NULL)
+	{
+		tokens[i] = tok;
+		tok = strtok(NULL, delim);
+		i++;
+	}
+	tokens[i] = NULL;
+	return (tokens);
 }
